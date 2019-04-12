4bed7b755b282e566f6588799894368e04519159
Brandon Lyday <33008415+bwlyday@users.noreply.github.com>
2019-04-12 15:15:32 -0700

    Added example JavaFX code to Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,41 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, World!");
+		launch(args);
 	}
+	public void start(Stage stage) throws Exception {
 
+		stage.setTitle("Section Searcher");
+		Label title = new Label("");
+		VBox root = new VBox();
+
+		Scene scene = new Scene(root, 1600, 800);
+		stage.setScene(scene);
+		
+		
+		Button button = new Button("Example Button");
+		button.setOnAction(new EventHandler<ActionEvent>()	{			
+			public void handle(ActionEvent event) {
+				System.out.println("You pushed the button.");
+			}		
+		});
+		title.setText("Section Searching is fun!");
+		root.getChildren().add(title);
+		root.getChildren().add(button);
+		stage.show();
+	}
+	
+	private void buttonSetup(Button button) {
+		
+	}
 }
