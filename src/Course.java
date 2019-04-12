afe5c097cf8076ceed1d6977fb4a415038b5e55b
Brandon Lyday <33008415+bwlyday@users.noreply.github.com>
2019-04-12 16:03:56 -0700

    Added TimeBlock and Time classes. Added basic set and get functionality to Course, Section, TimeBlock, and Time classes.

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Course {
-
+	private List<Section> sections;
+	private String name;
+	private long updateTime;
+	
+	public Course(String name, long updateTime) {
+		sections = new ArrayList<Section>();
+		this.name = name;
+		this.updateTime = updateTime;
+	}
+	
+	public String getName() {return this.name;}
+	
+	public void setName(String name) {this.name = name;}
+	
+	//TODO: implement isAvailable
+	public boolean isAvailable() {
+		return true;
+	}
+	
+	//TODO: implement update
+	//what does this even do???
+	public void update() {
+		return;
+	}
 }
--- src/Section.java
+++ src/Section.java
@@ -1,4 +1,18 @@
 
 public class Section {
-
+	private String id;	
+	private TimeBlock[] times; //each index corresponds to a day of the week. 0 = Sunday, 1 = Monday, 2 = Tuesday, etc.
+	
+	public Section(String id, TimeBlock[] times) {
+		this.id = id;
+		this.times = new TimeBlock[7];
+	}
+	
+	public String getID() {return this.id;}
+	
+	public void setID(String id) {this.id = id;}
+	
+	public TimeBlock[] getTimes() {return this.times;}
+	
+	public void setTimeBlock(TimeBlock[] times) {this.times = times;}
 }
