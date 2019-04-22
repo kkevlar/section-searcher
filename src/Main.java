3fd7cb387dfb750e391d551aec9060bfd5d3cfa0
Jacob Rakestraw <jrakestr@calpoly.edu>
2019-04-22 16:06:00 -0700

    Added more to main menu and styling

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,18 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -24,13 +32,23 @@ public class Main extends Application{
 
 		stage.setTitle("Section Searcher");
 		Label title = new Label("");
+		
+		MenuBar topMenu = new MenuBar();
+		
+		Menu homeLink = new Menu("Section Searcher");
+		homeLink.getStyleClass().clear();
+		homeLink.getStyleClass().add("home-link");
+		
+		topMenu.getMenus().addAll(homeLink);
+		
 		GridPane gridPane = new GridPane();
-
 	    gridPane.setPadding(new Insets(10,10,10,10));
-	    gridPane.setStyle("-fx-background-fill: black;");
-
-		Scene scene = new Scene(gridPane, 1600, 800);
+	    gridPane.getStyleClass().add("padded");
+	    
+		Scene scene = new Scene(new VBox(), 1600, 800);
+		scene.getStylesheets().add("Styles/Style.css");
 		stage.setScene(scene);
+		((VBox)scene.getRoot()).getChildren().addAll(topMenu, gridPane);
 		
 		Button button = new Button("Example Button");
 		button.setOnAction(new EventHandler<ActionEvent>()	{			
@@ -38,7 +56,8 @@ public class Main extends Application{
 				System.out.println("You pushed the button.");
 			}		
 		});
-	    gridPane.setAlignment(Pos.TOP_CENTER);
+	    gridPane.setAlignment(Pos.CENTER);
+	    gridPane.setMinHeight(700);
 		title.setText("Section Searching is fun!");
 		ColumnConstraints col1 = new ColumnConstraints();
         col1.setPercentWidth(25);
@@ -48,26 +67,60 @@ public class Main extends Application{
         col3.setPercentWidth(50);
         
         gridPane.getColumnConstraints().addAll(col1,col2,col3);
+        
+        RowConstraints row1 = new RowConstraints();
+        row1.setPercentHeight(10);
+        RowConstraints row2 = new RowConstraints();
+        row2.setPercentHeight(90);
+        
+        gridPane.getRowConstraints().addAll(row1, row2);
     
         col1.setHalignment(HPos.CENTER);
         col2.setHalignment(HPos.CENTER);
         col3.setHalignment(HPos.CENTER);
         
         Label label1 = new Label("Plans");
-        label1.setStyle("-fx-font-weight: 500; -fx-font-size: 24px;");
-        label1.setTextFill(Color.web("#0076a3"));
+        label1.getStyleClass().clear();
+        label1.getStyleClass().add("section-title");
 
         Label label2 = new Label("Categories");
-        label2.setStyle("-fx-font-weight: 500; -fx-font-size: 24px;");
-        label2.setTextFill(Color.web("#0076a3"));
+        label2.getStyleClass().clear();
+        label2.getStyleClass().add("section-title");
         
         Label label3 = new Label("Search");
-        label3.setStyle("-fx-font-weight: 500; -fx-font-size: 24px;");
-        label3.setTextFill(Color.web("#0076a3"));
+        label3.getStyleClass().clear();
+        label3.getStyleClass().add("section-title");
         
         gridPane.add(label3, 2, 0);
         gridPane.add(label2, 1, 0);
         gridPane.add(label1, 0, 0);
+        
+        GridPane pane1 = new GridPane();
+	    pane1.setAlignment(Pos.TOP_CENTER);
+        pane1.getStyleClass().add("borders");
+        pane1.getStyleClass().add("outside");
+        GridPane pane2 = new GridPane();
+	    pane2.setAlignment(Pos.TOP_CENTER);
+        pane2.getStyleClass().add("borders");
+        pane2.getStyleClass().add("outside");
+        GridPane pane3 = new GridPane();
+	    pane3.setAlignment(Pos.TOP_CENTER);
+        pane3.getStyleClass().add("borders");
+        pane3.getStyleClass().add("outside");
+        
+        TextField textField = new TextField();
+        textField.setPromptText("Search here!");
+        textField.requestFocus();
+        textField.getStyleClass().add("search-bar");
+        textField.getStyleClass().add("borders");
+        pane3.add(textField, 0, 0);
+        
+        TableView sectionTable = new TableView();
+        pane3.add(sectionTable, 0, 1);
+        
+        gridPane.add(pane1, 0, 1);
+        gridPane.add(pane2, 1, 1);
+        gridPane.add(pane3, 2, 1);
         
 		stage.show();
 	}
