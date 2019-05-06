b804e8c2625d07a01c521dacf484b12062877b31
Brandon Lyday <bwlyday@gmail.com>
2019-05-06 15:02:43 -0700

    Added more stuff to ClassDB Added comments Added extra constructor to construct ClassDB object with a predefined List<Course> object. Added function to remove courses Added function to return a list of all sections sorted by wait list Fixed sorting functions

--- src/logic/ClassDB.java
+++ src/logic/ClassDB.java
@@ -11,10 +11,19 @@ public class ClassDB {
 		courses = new ArrayList<Course>();
 	}
 	
+	public ClassDB(List<Course> courses) {
+		this.courses = courses;
+	}
+	
 	public void addCourse(Course course) {
 		this.courses.add(course);
 	}
 	
+	public void removeCourse(Course course) {
+		this.courses.remove(course);
+	}
+	
+	//gets all courses from this ClassDB object
 	public List<Course> getCourses(){
 		return this.courses;
 	}
@@ -41,8 +50,14 @@ public class ClassDB {
 		return sections;
 	}
 	
+	//gets all sections of a department from a List<Course> object
 	public static List<Section> getSectionsByDepartment(List<Course> courses, String department){
 		return getSections(filterDepartment(courses, department));
+	}
+	
+	//gets all sections of a department from this ClassDB object
+	public List<Section> getSectionsByDepartment(String department){
+		return getSections(filterDepartment(this.courses, department));
 	}
 	
 	//TODO: availability
@@ -50,10 +65,9 @@ public class ClassDB {
 		return null;
 	}
 	
-	
 	//TODO: degree
 	
-	//department
+	//returns a List<Course> filtered by department from the given List<Course> courses
 	public static List<Course> filterDepartment(List<Course> courses, String department){
 		List<Course> filtered = courses.stream()
 				.filter(course -> course.getDepartment().equals(department))
@@ -81,20 +95,28 @@ public class ClassDB {
 		return filtered;
 	}
 
+	
+	//returns a list of all sections sorted by wait list
+	public List<Section> sortWaitList(){
+		return sortWaitListSections(this.getSections());
+	}
+	
+	//returns a List<Section> sorted by lowest wait list to highest wait list
 	public static List<Section> sortWaitListCourses(List<Course> courses){
 		List<Section> sorted = getSections(courses);
 		
 		sorted = sorted.stream()
-				.sorted((s1, s2) -> s1.getWaitList() - s2.getWaitList())
+				.sorted((s1, s2) -> s2.getWaitList() - s1.getWaitList())
 				.collect(Collectors.toList());
 		return sorted;
 	}
 	
+	//returns a List<Section> sorted by lowest wait list to highest wait list
 	public static List<Section> sortWaitListSections(List<Section> sections){
 		List<Section> sorted;
 		
 		sorted = sections.stream()
-				.sorted((s1, s2) -> s1.getWaitList() - s2.getWaitList())
+				.sorted((s1, s2) -> s2.getWaitList() - s1.getWaitList())
 				.collect(Collectors.toList());
 		return sorted;
 	}
