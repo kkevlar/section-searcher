d31b86d582ebe7a9eacbaaa0af9bb1a164096d90
Kevin Kellar <kellar.kevin@gmail.com>
2019-05-24 15:06:03 -0700

    closes  #136

---
+++ src/test/scrapeinteractions/TestSuiteScrapeInteractions.java
@@ -0,0 +1,11 @@
+package test.scrapeinteractions;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestDepartmentLister.class, TestWebScraper.class })
+public class TestSuiteScrapeInteractions {
+
+}
---
+++ src/test/scrapeinteractions/TestWebScraper.java
@@ -0,0 +1,29 @@
+package test.scrapeinteractions;
+
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import logic.Course;
+import logic.WebScraper;
+
+public class TestWebScraper 
+{
+	@Test
+	public void testScrapeHasClasses()
+	{
+		WebScraper scraper = new WebScraper();
+		List<Course> course = scraper.scrapeCoursesByDept("CPE");
+		assertTrue(course.size() > 1);
+	}	
+	@Test
+	public void testScrapeHasCPE202()
+	{
+		WebScraper scraper = new WebScraper();
+		List<Course> course = scraper.scrapeCoursesByDept("CPE");
+		assertTrue(course.contains(new Course("CPE 202")));
+	}
+}
