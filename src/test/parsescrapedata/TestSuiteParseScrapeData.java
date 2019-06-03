451650f50c5bfdcd467665304c3be293b03a2d55
Justin Privitera <jprivite@calpoly.edu>
2019-06-03 14:17:27 -0700

    fixed smells in #92

---
+++ src/test/parsescrapedata/TestSuiteParseScrapeData.java
@@ -0,0 +1,11 @@
+package test.parsescrapedata;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestParseHtml.class, TestWebScraperParsing.class })
+public class TestSuiteParseScrapeData {
+
+}
